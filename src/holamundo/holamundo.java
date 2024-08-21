f61ac595dac3b789b8fe82318a400f28ef740fd9
angelsalcedo <angel.aguilar.salc@gmail.com>
2024-08-21 13:30:53 -0600

    Desafio2 Hola Global

--- src/holamundo/holamundo.java
+++ src/holamundo/holamundo.java
@@ -3,6 +3,6 @@ package holamundo;
 public class holamundo {
 	
 	public static void main(String[] args) {
-		System.out.print("Hola mundo");
+		System.out.print("Hola mundo, Hola Global Hitss");
 	}
 }
